3435907c52b46e88c35b9b360a085408a65b65e6
dudikpal <59420449+dudikpal@users.noreply.github.com>
2021-06-15 16:11:27 +0200

    service n1

--- game-app/src/main/java/game/GameService.java
+++ game-app/src/main/java/game/GameService.java
@@ -1,5 +1,8 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameService {
 
     private final GameRepository gameRepository;
@@ -7,6 +10,27 @@ public class GameService {
     public GameService(GameRepository gameRepository) {
         this.gameRepository = gameRepository;
     }
+
+    public Game biggestGoalDifference() {
+        int maxDifference = 0;
+        Game maxDiff = null;
+        for (Game game : gameRepository.games) {
+            int difference = Math.abs(game.firstCountryScore - game.secondCountryScore);
+            if (difference > maxDifference) {
+                maxDifference = difference;
+                maxDiff = game;
+            }
+        }
+        return maxDiff;
+    }
+
+    public int sumGoalOfCountry(String country) {
+        int sum = 0;
+        for (Game game : gameRepository.games) {
+            
+        }
+    }
+
 
 
 }
